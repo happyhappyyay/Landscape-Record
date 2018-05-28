0e2edfd4f6c651c46f681324e945b584b00e1175
happyhappyyay <chapan.denis@gmail.com>
2018-05-27 23:50:47 -0500

    Added log class

--- app/src/main/java/com/example/kingdenis/landscaperecord/LogActivity.java
+++ app/src/main/java/com/example/kingdenis/landscaperecord/LogActivity.java
@@ -3,15 +3,44 @@ package com.example.kingdenis.landscaperecord;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class LogActivity {
 
-    private int date;
-    private int time;
+    private long time;
+    private String username, addInfo;
+    private int logActivityAction, logActivityType;
 
-    public LogActivity(){
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        Date date = new Date();
+    public LogActivity(String username, String addInfo, int logActivityAction, int logActivityType){
+        time = System.currentTimeMillis();
+        this.username = username;
+        this.addInfo = addInfo;
+        this.logActivityAction = logActivityAction;
+        this.logActivityType = logActivityType;
+    }
+
+    @Override
+    public String toString() {
+        Date date = new Date(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.US);
+        String dateMessage = formatter.format(date);
+        return dateMessage + ": " + username + " " + convertActivityToString();
+    }
+
+    private String convertActivityToString() {
+        String activityAction = "";
+        List<String> logActivityList = Arrays.asList(LogActivityAction.ADD.toString(),
+                LogActivityAction.DELETE.toString(),LogActivityAction.UPDATE.toString());
+        List<String> logTypeList = Arrays.asList(LogActivityType.USER.toString(),
+                LogActivityType.ACCOUNT.toString(),LogActivityType.PAYMENT.toString());
+        activityAction += logActivityList.get(logActivityAction) + " ";
+        activityAction += logTypeList.get(logActivityType) + " ";
+        activityAction += addInfo;
+
+        return activityAction;
     }
 }
--- app/src/main/java/com/example/kingdenis/landscaperecord/LogActivityAction.java
+++ app/src/main/java/com/example/kingdenis/landscaperecord/LogActivityAction.java
@@ -2,7 +2,7 @@ package com.example.kingdenis.landscaperecord;
 
 public enum LogActivityAction {
     ADD,
-    REMOVE,
+    DELETE,
     UPDATE,
 
 }
